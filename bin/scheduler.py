76c466d9eb6316a9f391b587db71287fb9d5ff94
Simson Garfinkel <simsong@acm.org>
2025-07-02 05:04:21 -0400

    update

---
+++ bin/scheduler.py
@@ -0,0 +1,47 @@
+"""
+Runs every minute to implement rules.
+"""
+import datetime
+import json
+import asyncio
+from pprint import pprint
+
+
+import myapp.ae200 as ae200
+import myapp.app as app
+from myapp.ae200 import AE200Functions,AE200_ADDRESS
+
+#import rules
+#import lib.ctools as ctools
+import lib.ctools.clogging as clogging
+import lib.ctools.lock as clock
+
+address = AE200_ADDRESS
+
+def setup_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description='BasisTech LLC Rules Scheduler.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--debug", action='store_true')
+    parser.add_argument("--verbose", action='store_true')
+    parser.add_argument("--dry-run", action='store_true')
+    clogging.add_argument(parser)
+    return parser
+
+def main():
+    parser = setup_parser()
+    args = parser.parse_args()
+    clock.lock_script()
+    if args.dry_run:
+        print("=dry run=")
+
+if __name__=="__main__":
+    main()
+    now = datetime.datetime.now()
+    system_map = asyncio.run(app.system_map())
+    d = AE200Functions()
+    pprint(d.getDevices(address))
+    print("now=",now,"system_map=",system_map)
+    print(asyncio.run(ae200.get_erv_status()))
+    print("are 51 status:")
+    print(json.dumps(asyncio.run(ae200.get_dev_status(2)),indent=4))
---
+++ tests/test_db.py
@@ -0,0 +1,62 @@
+#import asyncio
+import logging
+from unittest.mock import AsyncMock, patch
+import sqlite3
+import os
+#import time
+import pytest_asyncio
+import pytest
+import tempfile # Import tempfile
+#import shutil   # Import shutil for directory cleanup
+
+from fastapi.testclient import TestClient
+# from contextlib import asynccontextmanager # Not directly used on override_get_db_connection
+
+from myapp.main import app as fastapi_app
+import myapp.ae200 as ae200
+import myapp.aqi as aqi
+import myapp.db as db
+from myapp.paths import SCHEMA_FILE_PATH
+#from myapp.main import status, set_speed, SpeedControl
+
+logger = logging.getLogger(__name__)
+
+# Optional: enable pytest-asyncio
+pytest_plugins = ("pytest_asyncio",)
+
+@pytest.fixture
+def db_conn():
+    """Clean database connection to a database that is created for the purpose"""
+    with tempfile.NamedTemporaryFile(suffix=".db") as tf:
+        conn = sqlite3.connect(tf.name)
+        conn.row_factory = sqlite3.Row      # returns rows as dicts
+        conn.execute("PRAGMA foreign_keys = ON;") # Ensure foreign keys are enabled
+        cursor = conn.cursor()
+        db.setup_database(conn, SCHEMA_FILE_PATH)
+        yield conn
+
+def test_temperature_insert(db_conn):
+    db.insert_devlog_entry(db_conn, "devtest1", temp=20, logtime=100)
+    db.insert_devlog_entry(db_conn, "devtest2", temp=20, logtime=100)
+    db.insert_devlog_entry(db_conn, "devtest1", temp=20, logtime=101)
+    db.insert_devlog_entry(db_conn, "devtest2", temp=21, logtime=111)
+    db.insert_devlog_entry(db_conn, "devtest1", temp=20, logtime=112)
+    db.insert_devlog_entry(db_conn, "devtest2", temp=22, logtime=112)
+
+    dev1_id = db.get_or_create_device_id(db_conn, "devtest1")
+    dev2_id = db.get_or_create_device_id(db_conn, "devtest2")
+    assert dev1_id != dev2_id
+
+    c = db_conn.cursor()
+    c.execute("SELECT *,dn.name as name from devlog d1 INNER JOIN (select device_id,MAX(logtime) as max_logtime from devlog group by device_id) as d2 on d1.device_id = d2.device_id and d1.logtime = d2.max_logtime INNER JOIN device_names dn on d1.device_id = dn.id")
+    rows = c.fetchall()
+    assert len(rows)==2
+    assert rows[0]['name'] == 'devtest1'
+    assert rows[0]['temp10x'] == 200
+    assert rows[0]['logtime'] == 100
+    assert rows[0]['duration'] == 13
+
+    assert rows[1]['name'] == 'devtest2'
+    assert rows[1]['temp10x'] == 220
+    assert rows[1]['logtime'] == 112
+    assert rows[1]['duration'] == 1
