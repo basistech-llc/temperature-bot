10276f5f11f73960c0423cf1577ea685ab1e5458
Simson Garfinkel <simsong@basistech.com>
2025-08-31 20:25:47 +0000

    updated buttons

--- app/main.py
+++ app/main.py
@@ -344,20 +344,6 @@ def read_index(conn):
 
     return render_template("index.html", develop=DEV, devices=device_data, now=now)
 
-
-@app.route("/privacy")
-def privacy():
-    return render_template("privacy.html")
-
-
-@app.route("/version")
-def get_version():
-    return f"version: {__version__}"
-
-@app.route("/logs")
-def do_logs():
-    return render_template("logs.html")
-
 @app.route("/rules")
 @with_db_connection
 def show_rules(conn):
@@ -393,6 +379,9 @@ def show_rules(conn):
         times=rules_engine.get_time_dict(),
     )
 
+@app.route("/logs")
+def do_logs():
+    return render_template("logs.html")
 
 @app.route("/device_log/<device_id>")
 @with_db_connection
@@ -441,6 +430,18 @@ def show_chart():
 
     return render_template("chart.html", device_ids=device_ids)
 
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/buttons")
+def buttons():
+    return render_template("buttons.html")
+
+@app.route("/version")
+def get_version():
+    return f"version: {__version__}"
 
 # Error handler
 @app.errorhandler(HTTPException)
